b588fcfb2678a974a5a7304b51ff24a3fac9d161
duyenle1312 <81138129+duyenle1312@users.noreply.github.com>
2021-07-29 19:52:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // init sqlite db
-const dbFile = "./.data/users.db";
+const dbFile = "./.data/data.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
